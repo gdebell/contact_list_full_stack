a907949f65e8e8a58e8ef0d4d096c1a0c0f75142
Gina <gdebell@gmail.com>
2016-09-02 13:15:52 -0600

    set up node/express application with  boiler plate.  create a contact route that send static html.  return dummy data on page

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const contactObj= require('../lib/data').all;
 
 const indexController = require('../controllers/index');
 
 router.get('/', function (req, res, next) {
   const renderObject = {};
   renderObject.title = 'Welcome to Express!';
-  indexController.sum(1, 2, (error, results) => {
+  indexController.sum(1, 6, (error, results) => {
     if (error) return next(error);
     if (results) {
       renderObject.sum = results;
       res.render('index', renderObject);
     }
   });
+});
+
+router.get('/contacts', function (req, res, next) {
+  res.render('contacts', {contacts: contactObj} );
 });
 
 module.exports = router;
